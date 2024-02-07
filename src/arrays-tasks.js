2a24fa91c0d84c7e4c46720a20c94a2f8d712e55
Roisler <roislerxs@gmail.com>
2024-02-08 00:18:51 +0700

    feat: solve swapHeadAndTail

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -594,8 +594,20 @@ function shiftArray(arr, n) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const mapping = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+  return arr.sort((a, b) => mapping.indexOf(a) - mapping.indexOf(b));
 }
 
 /**
@@ -617,8 +629,14 @@ function sortDigitNamesByNumericOrder(/* arr */) {
  *   swapHeadAndTail([]) => []
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const newLengthHeadAndTail = Math.floor(arr.length / 2);
+  const head = arr.splice(0, newLengthHeadAndTail);
+  const tail = arr.splice(-newLengthHeadAndTail);
+  return [...tail, ...arr, ...head];
 }
 
 module.exports = {
